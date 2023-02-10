6baf1f5fbfb0bad215f92c0126d9667877502839
Anthony Laplane <anthony.laplane@gmail.com>
2023-02-10 10:07:32 +0100

    live 01

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 $recipes = [
-    ['title' => 'Mousse au chocolat', 'description' => 'test mousse', 'image' => '1-chocolate-au-mousse.jpg'],
-    ['title' => 'Gratin', 'description' => 'test gratin', 'image' => '2-gratin-dauphinois.jpg'],
-    ['title' => 'Salade', 'description' => 'test salade', 'image' => '3-salade.jpg'],
+    ['title' => 'Mousse au chocolat', 'description' => 'test mousse Lorem ipsum dolor sit amet consectetur adipisicing elit.', 'image' => '1-chocolate-au-mousse.jpg'],
+    ['title' => 'Gratin dauphinois', 'description' => 'test gratin Lorem ipsum dolor sit amet consectetur adipisicing elit.', 'image' => '2-gratin-dauphinois.jpg'],
+    ['title' => 'Salade de chèvre', 'description' => 'test salade Lorem ipsum dolor sit amet consectetur adipisicing elit.', 'image' => '3-salade.jpg'],
 ];
 
 ?>
@@ -43,36 +43,41 @@ $recipes = [
                 <button type="button" class="btn btn-primary">Sign-up</button>
             </div>
         </header>
-
-        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-            <div class="col-10 col-sm-8 col-lg-6">
-                <img src="assets/images/logo-cuisinea.jpg" class="d-block mx-lg-auto img-fluid" alt="Cuisinea" width="400" loading="lazy">
-            </div>
-            <div class="col-lg-6">
-                <h1 class="display-5 fw-bold lh-1 mb-3">Recettes de cuisines pour tout le monde</h1>
-                <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Voir toutes nos recettes</button>
+        <main>
+            <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+                <div class="col-10 col-sm-8 col-lg-6">
+                    <img src="assets/images/logo-cuisinea.jpg" class="d-block mx-lg-auto img-fluid" alt="Cuisinea" width="400" loading="lazy">
                 </div>
-            </div>
-        </div>
-
-        <div class="row text-center">
-            <?php foreach ($recipes as $recipe) { ?>
-                <div class="col-md-4 my-2 d-flex">
-                    <div class="card">
-                        <img src="uploads/recipes/<?=$recipe['image'] ?>" class="card-img-top" alt="<?=$recipe['title'] ?>">
-                        <div class="card-body">
-                            <h5 class="card-title"><?=$recipe['title'] ?></h5>
-                            <p class="card-text"><?=$recipe['description'] ?></p>
-                            <a href="#" class="btn btn-primary">Voir la recette</a>
-                        </div>
+                <div class="col-lg-6">
+                    <h1 class="display-5 fw-bold lh-1 mb-3">Recettes de cuisines pour tout le monde</h1>
+                    <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                        <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Voir toutes nos recettes</button>
                     </div>
                 </div>
-            <?php } ?>
+            </div>
 
-        </div>
+            <div class="row text-center">
+                <?php foreach ($recipes as $recipe) { ?>
+                    <div class="col-md-4 my-2 d-flex">
+                        <div class="card">
+                            <img src="uploads/recipes/<?= $recipe['image'] ?>" class="card-img-top" alt="<?= $recipe['title']; ?>">
 
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $recipe['title'] ?></h5>
+                                <p class="card-text"><?= $recipe['description'] ?></p>
+                                <a href="#" class="btn btn-primary">Voir la recette</a>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+
+            </div>
+
+
+
+            
+        </main>
         <footer class="py-3 my-4">
             <ul class="nav justify-content-center border-bottom pb-3 mb-3">
                 <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
